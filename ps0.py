9b9a60c9cd13a0cec3443fca3a1ecea42e60d0fc
Michael Sun <msun2019@pingry.org>
2016-03-31 08:54:16 -0400

    All 9 Problems working properly.

--- ps0.py
+++ ps0.py
@@ -1,9 +1,9 @@
 # Problem 0
 def is_even(number):
 	if number % 2 == 1:
-		answer = "Odd"
+		answer = "False"
 	else:
-		answer = "Even"
+		answer = "True"
 	return answer 
 	
 #Problem 1
@@ -25,7 +25,6 @@ def sum_less_ints(number):
 	real_number = number
 	number -= 1
 	list = ["Value"] * number 
-	print(list)
 	numberList = []
 	answer = 0 
 	for item in list:
@@ -35,6 +34,83 @@ def sum_less_ints(number):
 		answer += int(value)
 	return answer 
 	
+#Problem 4
+def factorial(number):
+	list = ["Value"] * number
+	numberList = []
+	answer = 1
+	for item in list:
+		numberList.append(number)
+		number -= 1
+	for value in numberList:
+		answer *= int(value)
+	return answer 
+
+#Problem 5
+def factor(x, y):
+	if x % y == 0:
+		answer = "True"
+	else:
+		answer = "False"
+	return answer 
+
+#Problem 6
+def test_prime(number):
+	root = number ** .5
+	list = range(2, int(root) + 1)
+	answer = " "
+	for item in list:
+		if number % int(item) == 0:
+			answer = "Not Prime"
+			break 
+		if number % int(item) > 0:
+			answer = "Prime"
+	if number == 2:
+		answer = "Prime"
+	if number == 1:
+		answer = "Not Prime"
+	return answer
+	
+#Problem 7
+def perfect(number):
+	list = range(1, number)
+	number_list = []
+	value = 0
+	for item in list:
+		if number % item == 0:
+			number_list.append(item)
+	for item in number_list:
+		value += item 
+	if value == number:
+		answer = "Perfect Number"
+	else:
+		answer = "Not a Perfect Number"
+	return answer
+	
+#Problem 8 
+def sum_digits_divide(number):
+	if number % sum_digits(number) == 0:
+		answer = "True"
+	else:
+		answer = "False"
+	return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 		
